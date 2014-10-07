ec4fcedfbb204cadbb4c3a38992c111a6f79daeb
Snaipe <franklinmathieu@gmail.com>
2014-10-07 20:41:03 +0200

    Added main

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+#include "iso.h"
+#include "shell.h"
+
+static int print_usage(char *arg0)
+{
+    printf("usage: %s <iso file>", arg0);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+        return print_usage(argv[0]);
+
+    struct iso iso;
+    iso_load(argv[1], &iso);
+
+    run(&iso);
+
+    iso_release(&iso);
+    return 0;
+}
