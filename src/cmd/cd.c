12abc60189fbb7049ed75e2f1a2aa9e3b7da36b4
Snaipe <franklinmathieu@gmail.com>
2014-10-09 00:36:31 +0200

    Added cd command

---
+++ src/cmd/cd.c
@@ -0,0 +1,31 @@
+#include <errno.h>
+#include <string.h>
+#include <stdio.h>
+
+#include "cd.h"
+#include "../dir.h"
+
+static const char *get_message(void *result)
+{
+    if (!result)
+    {
+        switch (errno)
+        {
+        case ENOENT:
+            return "unable to find '%s' directory entry\n";
+        case ENOTDIR:
+            return "entry '%s' is not a directory\n";
+        default:
+            return "an unexpected error occured while trying to cd to '%s'\n";
+        }
+    }
+    else
+        return "Changing to '%s' directory\n";
+}
+
+void cd(struct iso *context, int argc, char *argv[])
+{
+    char *dirname = "root dir";
+    void *res = setcwd(context, argc < 2 ? NULL : (dirname = argv[1]));
+    printf(get_message(res), dirname);
+}
---
+++ src/cmd/cd.h
@@ -0,0 +1,8 @@
+#ifndef CD_H_
+# define CD_H_
+
+#include "../iso.h"
+
+void cd(struct iso *context, int argc, char *argv[]);
+
+#endif /* !CD_H_ */
