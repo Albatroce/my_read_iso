8517b08ca7d2641bf0367067e8f5569179448c18
Snaipe <franklinmathieu@gmail.com>
2014-10-11 23:23:20 +0200

    Added documentation for shell.h and iso.h functions

--- src/iso.h
+++ src/iso.h
@@ -6,17 +6,48 @@
 
 struct iso
 {
-    int fd;
-    void *map;
-    size_t size;
-    struct iso_dir *cwd;
+    int fd;              /** The file descriptor bound to the iso file */
+    void *map;           /** The mapped iso file */
+    size_t size;         /** The size of the map */
+    struct iso_dir *cwd; /** The current work directory */
 };
 
+/**
+** Loads an iso file from a filename
+** @param[in] filename: the path to the file to load
+** @param[out] iso: the iso context to populate
+** @returns the newly populated iso context, or NULL if an error occured.
+*/
 struct iso *iso_load(const char *filename, struct iso *iso);
+
+/**
+** Loads an iso file from a file descriptor
+** @param[in] fd: the file descriptor of the file to load
+** @param[out] iso: the iso context to populate
+** @returns the newly populated iso context, or NULL if an error occured.
+*/
 struct iso *iso_load_fd(int fd, struct iso *iso);
+
+/**
+** Unloads the iso context
+** @param[in] iso: the iso context to populate
+*/
 void iso_release(struct iso *iso);
+
+/**
+** Loads an iso file from a filename
+** @param[in] filename: the path to the file to load
+** @param[out] iso: the iso context to populate
+** @returns the newly populated iso context, or NULL if an error occured.
+*/
 struct iso_prim_voldesc *iso_describe(struct iso *iso);
 
+/**
+** Returns a pointer to the begining of the nth section
+** @param[in] context: the iso context
+** @param[in] sector_num: the sector number to seek
+** @returns the pointer to the begining of the section
+*/
 __attribute__ ((always_inline))
 inline void *iso_sector(struct iso *context, size_t sector_num)
 {
--- src/shell.h
+++ src/shell.h
@@ -20,7 +20,21 @@
     "cat\t: display file content\n"         \
     "quit\t: program exit\n"
 
+/**
+** The main shell function -- reads commands from stdin until no more bytes
+** are available or the 'quit' command is given.
+** @param[in] context: the iso context
+*/
 void run(struct iso *context);
+
+/**
+** Validates that the argument size are as expected, and prints an error
+** otherwise.
+** @param[in] name: the name of the command (purely cosmetic for the message)
+** @param[in] actual: the actual argument count
+** @param[in] expected: the expected argument count
+** @returns 1 if the command has been validated, 0 othervise
+*/
 int validate_cmd_args(const char *name, int actual, int expected);
 
 #endif /* !SHELL_H_ */
